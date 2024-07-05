b45bf9874d0db6f91792afa22f0b7d9731531f84
clariserni <clarissaserni.96@gmail.com>
2024-07-05 12:28:08 +0200

    first commit

---
+++ src/main/java/com/conto_bancario/ContoBancarioApplication.java
@@ -0,0 +1,13 @@
+package com.conto_bancario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContoBancarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContoBancarioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/conto_bancario/ContoBancarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.conto_bancario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContoBancarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
